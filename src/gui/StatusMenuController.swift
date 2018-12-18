67ebfdef412a465d32123b75291a9d426fac121b
Michal Duda <github@vookimedlo.cz>
2018-12-18 21:35:13 +0100

    GUI: Monitoring switch

--- src/gui/StatusMenuController.swift
+++ src/gui/StatusMenuController.swift
@@ -9,14 +9,29 @@
 import Cocoa
 import Foundation
 
-class StatusMenuController: NSObject {
+class StatusMenuController: NSObject, MonitoringToggledHandler {
     @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var toggleMonitoringViewController: ToggleMonitoringViewController!
     
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     override func awakeFromNib() {
+        let monitoringPlaceHolder = statusMenu.item(withTag: 1)
+        monitoringPlaceHolder?.view = toggleMonitoringViewController.view
+        
         statusItem.button?.title = Bundle.appName()
         statusItem.menu = statusMenu
+        
+        startObserving()
+    }
+    
+    deinit {
+        stopObserving()
+    }
+    
+    func monitoringToggled(notification: Notification) {
+        guard notification.name == Notification.Name.CustomMonitoringToggled else {return}
+        logDebug("monitoring %@", notification.userInfo?["isEnabled"] as! Bool ? "enabled" : "disabled")
     }
     
     @IBAction func aboutAction(_ sender: NSMenuItem) {
---
+++ src/gui/ToggleMonitoringViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleMonitoringViewController.swift
+//  ck550
+//
+//  Created by Michal Duda on 18/12/2018.
+//  Copyright © 2018 Michal Duda. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ToggleMonitoringViewController: ViewController {
+    @IBAction func toggledAction(_ sender: NSSegmentedCell) {
+        let userInfo = ["isEnabled": sender.isSelected(forSegment: 0)]
+        let notification = Notification(name: .CustomMonitoringToggled, object: self, userInfo: userInfo)
+        NotificationCenter.default.post(notification)
+    }
+}
