c5c1b4c93fca5f23a1b2e96cd6ab1ebf6d56a8b0
Michal Duda <github@vookimedlo.cz>
2020-11-28 16:37:14 +0100

    🏗Build: Notarization support of the sparkle's autoupdate

--- src/scripts/sign-noUpdater.sh
+++ src/scripts/sign-noUpdater.sh
@@ -7,6 +7,9 @@ LANG="C"
 
 cd ../build-noupdate/Build/Products/Release
 
+codesign --force --verify --verbose -o runtime --deep --sign  "${DEVELOPER_IDENTITY_CK550}" --options runtime --verbose ./ck550.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app/Contents/MacOS/fileop
+codesign --force --verify --verbose -o runtime --deep --sign  "${DEVELOPER_IDENTITY_CK550}" --options runtime --verbose ./ck550.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
+
 find ck550.app -name "*.dylib" | xargs codesign --force --verify --verbose --sign "${DEVELOPER_IDENTITY_CK550}"
 find ck550.app -name "*.framework" | xargs codesign --force --verify --verbose --sign "${DEVELOPER_IDENTITY_CK550}"
 
